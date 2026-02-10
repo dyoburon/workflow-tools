bba38725ec26081b12172bd6e129db7b138191ba
Dylan <burtond@uw.edu>
2026-02-09 22:13:45 -0800

    image data

--- image-to-clipboard-for-claude/init.lua
+++ image-to-clipboard-for-claude/init.lua
@@ -1,20 +1,13 @@
--- Screenshot to Clipboard Path (Cmd+Option+S)
--- Takes interactive screenshot, saves to file, copies PATH to clipboard
+-- Screenshot to Clipboard (Cmd+Option+S)
+-- Takes interactive screenshot, copies image data to clipboard
 -- Requires: Hammerspoon (https://www.hammerspoon.org/)
 
-local screenshotDir = os.getenv("HOME") .. "/Documents/Screenshots"
-hs.fs.mkdir(screenshotDir)
-
 hs.hotkey.bind({"cmd", "alt"}, "S", function()
-    local timestamp = os.date("%Y-%m-%d_%H-%M-%S")
-    local filename = screenshotDir .. "/screenshot_" .. timestamp .. ".png"
-
     local task = hs.task.new("/usr/sbin/screencapture", function(exitCode)
         if exitCode == 0 then
-            hs.pasteboard.setContents(filename)
-            hs.alert.show("Path copied!")
+            hs.alert.show("Screenshot copied!")
         end
-    end, {"-i", "-x", filename})
+    end, {"-c", "-i", "-x"})
 
     task:start()
 end)
